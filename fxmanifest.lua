d1b574e169fe0e74c7559c8bb137476239e91771
RexshackGaming <rexshackuk@gmail.com>
2024-11-01 20:22:21 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,5 @@
 -- FX Information
 fx_version "cerulean"
-use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
